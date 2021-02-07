f29a71e8b3b1bdc0750e85565d826af264920320
giegeralexandra <giegeralexandra@gmail.com>
2021-02-07 10:55:22 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
-document.addEventListener("DOMContentLoaded", () => {
+//document.addEventListener("DOMContentLoaded", () => {
   // your code here
-});
+//});
+
+document.getElementById('create-task-form').addEventListener("submit", function(event) {
+  let ul = document.querySelector('ul')
+  let li = document.createElement('li')
+  li.innerHTML += `${document.getElementById("new-task-description").value}`
+  document.getElementById("tasks").appendChild(li)
+  event.preventDefault();
+}, false);
+
+
